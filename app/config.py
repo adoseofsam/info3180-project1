30ea96af029cfa2759439516d601a3b4cd1b066d
samanthajames <jamessamantha099@gmail.com>
2021-03-24 00:14:22 -0500

    Complete

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'R@nd0mK*y'
     SQLALCHEMY_DATABASE_URI =  os.environ.get('APP_DATABASE_URL') 
     # or 'postgresql://adoseofsam:project1pwd@localhost/webproject1'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './uploads'
     
 
